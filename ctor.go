a2f61d495f60164f9edc4e304fe1ea5ddb801e6e
Hyori 효리 <hyorigo@icloud.com>
2024-01-16 00:18:23 +0800

    basic ctor new

--- ctor.go
+++ ctor.go
@@ -1,1 +1,28 @@
 package starbox
+
+import (
+	"fmt"
+	"time"
+
+	"bitbucket.org/ai69/amoy"
+	"github.com/1set/starlet"
+	"go.starlark.net/starlark"
+)
+
+// Starbox is a wrapper of starlet.Machine with additional features.
+type Starbox struct {
+	*starlet.Machine
+}
+
+// NewStarbox creates a new Starbox instance with default settings.
+func NewStarbox(name string) *Starbox {
+	m := starlet.NewDefault()
+	m.EnableGlobalReassign()
+	m.SetInputConversionEnabled(false)
+	m.SetOutputConversionEnabled(true)
+	m.SetPrintFunc(func(thread *starlark.Thread, msg string) {
+		prefix := fmt.Sprintf("[⭐|%s](%s)", name, time.Now().UTC().Format(`15:04:05.000`))
+		amoy.Eprintln(prefix, msg)
+	})
+	return &Starbox{m}
+}
