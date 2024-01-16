beef8481ff297e3a8600466125a2e803a4c94388
Hyori 효리 <hyorigo@icloud.com>
2024-01-16 14:38:27 +0800

    refine ctor

--- ctor.go
+++ ctor.go
@@ -2,6 +2,7 @@ package starbox
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"bitbucket.org/ai69/amoy"
@@ -12,6 +13,8 @@ import (
 // Starbox is a wrapper of starlet.Machine with additional features.
 type Starbox struct {
 	*starlet.Machine
+	mu     sync.RWMutex
+	hasRun bool
 }
 
 // NewStarbox creates a new Starbox instance with default settings.
@@ -24,10 +27,34 @@ func NewStarbox(name string) *Starbox {
 		prefix := fmt.Sprintf("[⭐|%s](%s)", name, time.Now().UTC().Format(`15:04:05.000`))
 		amoy.Eprintln(prefix, msg)
 	})
-	return &Starbox{m}
+	return &Starbox{Machine: m}
 }
 
 // SetTag sets the custom tag of Go struct fields for Starlark.
 func (s *Starbox) SetTag(tag string) {
 	s.Machine.SetCustomTag(tag)
+}
+
+// SetKeyValue adds a key-value pair to the global environment before running.
+// It panics if called after running.
+func (s *Starbox) SetKeyValue(key string, value interface{}) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.hasRun {
+		log.DPanic("cannot add key-value pair after running")
+	}
+	s.Machine.AddGlobals(starlet.StringAnyMap{key: value})
+}
+
+// SetKeyValues adds key-value pairs to the global environment before running.
+// It panics if called after running.
+func (s *Starbox) SetKeyValues(keyValues starlet.StringAnyMap) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.hasRun {
+		log.DPanic("cannot add key-value pairs after running")
+	}
+	s.Machine.AddGlobals(keyValues)
 }
