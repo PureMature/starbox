cb139654bae3aa10709d0761c7fc5635d69f104a
Hyori 효리 <hyorigo@icloud.com>
2024-01-23 22:08:16 +0800

    for add funcs

--- ctor.go
+++ ctor.go
@@ -8,13 +8,19 @@ import (
 	"bitbucket.org/ai69/amoy"
 	"github.com/1set/starlet"
 	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
 )
+
+// StarlarkFunc is a function that can be called from Starlark.
+type StarlarkFunc func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error)
 
 // Starbox is a wrapper of starlet.Machine with additional features.
 type Starbox struct {
 	*starlet.Machine
-	mu     sync.RWMutex
-	hasRun bool
+	mu        sync.RWMutex
+	hasRun    bool
+	builtMods []string
+	loadMods  map[string]starlet.ModuleLoader
 }
 
 // NewStarbox creates a new Starbox instance with default settings.
@@ -35,9 +41,10 @@ func (s *Starbox) SetTag(tag string) {
 	s.Machine.SetCustomTag(tag)
 }
 
-// SetKeyValue adds a key-value pair to the global environment before running.
+// AddKeyValue adds a key-value pair to the global environment before running.
+// If the key already exists, it will be overwritten.
 // It panics if called after running.
-func (s *Starbox) SetKeyValue(key string, value interface{}) {
+func (s *Starbox) AddKeyValue(key string, value interface{}) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
@@ -47,9 +54,10 @@ func (s *Starbox) SetKeyValue(key string, value interface{}) {
 	s.Machine.AddGlobals(starlet.StringAnyMap{key: value})
 }
 
-// SetKeyValues adds key-value pairs to the global environment before running.
+// AddKeyValues adds key-value pairs to the global environment before running.
+// For each key-value pair, if the key already exists, it will be overwritten.
 // It panics if called after running.
-func (s *Starbox) SetKeyValues(keyValues starlet.StringAnyMap) {
+func (s *Starbox) AddKeyValues(keyValues starlet.StringAnyMap) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
@@ -58,3 +66,73 @@ func (s *Starbox) SetKeyValues(keyValues starlet.StringAnyMap) {
 	}
 	s.Machine.AddGlobals(keyValues)
 }
+
+// AddBuiltin adds a builtin function with name to the global environment before running.
+// If the name already exists, it will be overwritten.
+// It panics if called after running.
+func (s *Starbox) AddBuiltin(name string, starFunc StarlarkFunc) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.hasRun {
+		log.DPanic("cannot add builtin after running")
+	}
+	sb := starlark.NewBuiltin(name, starFunc)
+	s.Machine.AddGlobals(starlet.StringAnyMap{name: sb})
+}
+
+// AddNamedModules adds builtin modules by name to the preload and lazyload registry.
+// It will not load the modules until the first run.
+func (s *Starbox) AddNamedModules(moduleNames ...string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.builtMods = append(s.builtMods, moduleNames...)
+}
+
+// AddModuleLoader adds a custom module loader to the preload and lazyload registry.
+// It will not load the module until the first run.
+func (s *Starbox) AddModuleLoader(moduleName string, moduleLoader starlet.ModuleLoader) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.loadMods == nil {
+		s.loadMods = make(map[string]starlet.ModuleLoader)
+	}
+	s.loadMods[moduleName] = moduleLoader
+}
+
+// AddModuleData creates a module for the given module data along with a module loader, and adds it to the preload and lazyload registry.
+func (s *Starbox) AddModuleData(moduleName string, moduleData starlark.StringDict) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.loadMods == nil {
+		s.loadMods = make(map[string]starlet.ModuleLoader)
+	}
+	s.loadMods[moduleName] = func() (starlark.StringDict, error) {
+		sm := starlarkstruct.Module{Name: moduleName, Members: moduleData}
+		return starlark.StringDict{
+			moduleName: &sm,
+		}, nil
+	}
+}
+
+/*
+pre, err := starlet.MakeBuiltinModuleLoaderList(moduleNames...)
+if err != nil {
+	panic(err)
+}
+lazy, err := starlet.MakeBuiltinModuleLoaderMap(moduleNames...)
+if err != nil {
+	panic(err)
+}
+s.Machine.SetPreloadModules(pre)
+s.Machine.SetLazyloadModules(lazy)
+
+SetPreloadModules ... by system names
+SetLazyloadModules ... by system names ... exists?
+
+SetCustomPreloadModules for real modules
+SetCustomLazyloadModules for real modules
+*/
--- exec.go
+++ exec.go
@@ -1,1 +1,6 @@
 package starbox
+
+var (
+	// defaultSafeModules is the list of safe modules.
+	defaultSafeModules = []string{"base64", "go_idiomatic", "hashlib", "http", "json", "math", "random", "re", "struct", "time"}
+)
